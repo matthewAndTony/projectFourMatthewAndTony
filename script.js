0c90876347523e20c5b86e71f3fa2fcfdb50691c
mseneoudom <matt_sen@live.ca>
2020-04-16 18:15:41 -0400

    added some functions to call

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const stockApp = {};
 stockApp.apiKey = `I57N7ZBNDACAALUE`;
-
+const apiCall =
+    $.ajax({
+        url:`https://www.alphavantage.co/query`,
+        method:`GET`,
+        dataType:`json`,
+})
+// console.log(apiCall);
+apiCall.data={
+    function: `SYMBOL_SEARCH`,
+    keywords: 'bam',
+    apikey: stockApp.apiKey
+}
+console.log(apiCall.data)
 
 // Search Endpoint Suggestions --- CREATE DIV/LI BELOW forEACH result?
 stockApp.searchEndpoint = function(search){
@@ -48,12 +60,32 @@ stockApp.forEX = function(from,to){
             apikey: stockApp.apiKey,
         }
     }).then(function(results){
-        // console.log(results);
+        console.log(results);
     })
 }
 
+// Technical Indicators
+stockApp.techIndicate = function(symbol, interval, timePeriod, seriesType){
+    $.ajax({
+        url:`https://www.alphavantage.co/query`,
+        method:`GET`,
+        dataType:`json`,
+        data:{
+            function: `SMA`,
+            symbol:symbol,
+            interval: interval,
+            time_period:timePeriod,
+            series_type: seriesType,
+            apikey: stockApp.apiKey,
+        }
+    }).then(function(results){
+        console.log(results);
+    })
+}
 
 $(function(){
-    stockApp.init(`tsla`,`1min`, `intraday`)
-    stockApp.search('goo');
+    stockApp.searchEndpoint('brookfield');
+    // stockApp.searchStock('msft','1min','intraday');
+    // stockApp.forEX('CAD','USD')
+    // stockApp.techIndicate('tsla', '1min', 60, 'open');
 })
