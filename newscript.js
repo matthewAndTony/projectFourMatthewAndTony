280c3e8ac811675bc55e90420411625bfb4ca54d
tony salloum <antoine.salloum@gmail.com>
2020-04-19 14:25:45 -0400

    js updated

--- newscript.js
+++ newscript.js
@@ -1,20 +1,15 @@
 const newsApp = {};
 newsApp.apiKey = `f9648353dfc841bb861714d68d34c9c2`;
-
 const stockApp = {};
 stockApp.apiKey = `I57N7ZBNDACAALUE`;
-
 const addedToWatchList = {};
-const watchListSearchItems = [];
-
-
 // End Point API Search - When the user submits or clicks the submit button, it will the search suggestions back to the user.
-stockApp.searchEndpoint = function(search){
+stockApp.searchEndpoint = function (search) {
     $.ajax({
-        url:`https://www.alphavantage.co/query`,
-        method:`GET`,
-        dataType:`json`,
-        data:{
+        url: `https://www.alphavantage.co/query`,
+        method: `GET`,
+        dataType: `json`,
+        data: {
             function: `SYMBOL_SEARCH`,
             keywords: search,
             apikey: stockApp.apiKey,
@@ -33,66 +28,49 @@ stockApp.searchEndpoint = function(search){
         })
     })
 }
-
-//Ticker Information API - When the user selects which ticker they would like to add by clicking, the ticker's information will be displayed in an LI under the watch list.
-
-stockApp.searchStock = function(ticker,timeSeries, stock, currency){
+//Ticker Information API - When the user selects which ticker they would like to add by clicking, the ticker's information will be displayed in an LI under the watchlist.
+stockApp.searchStock = function (ticker, timeSeries, stock, currency) {
     $.ajax({
-        url:`https://www.alphavantage.co/query`,
-        method:`GET`,
-        dataType:`json`,
-        data:{
+        url: `https://www.alphavantage.co/query`,
+        method: `GET`,
+        dataType: `json`,
+        data: {
             function: `TIME_SERIES_${timeSeries.toUpperCase()}`,
             symbol: ticker.toUpperCase(),
             apikey: stockApp.apiKey,
         }
-    }).then(function(results){
+    }).then(function (results) {
         const lastRefresh = results['Meta Data']["3. Last Refreshed"];
-    
-        const todaysResults = results[`Time Series (Daily)`][lastRefresh]        
-        
+        const todaysResults = results[`Time Series (Daily)`][lastRefresh]
         const todaysOpen = todaysResults['1. open']; //Get Open Price
         const todaysClose = todaysResults['4. close']; //Get Closing Price
         const todaysChanges = todaysOpen - todaysClose; //Get Change in Dollars
-        const todaysChangesPercent = (todaysOpen - todaysClose)/todaysOpen; //Get Change in Percent
+        const todaysChangesPercent = (todaysOpen - todaysClose) / todaysOpen; //Get Change in Percent
         const todaysVolume = todaysResults['5. volume']; //Get Volume        
-
-
-
-        watchListSearchItems.push(stock, ticker)//Create an array of items to search 
-        watchListSearchItems.forEach(function(item){ //News Api Search for Each item in the Array
-            newsApp.init(item);
-        })
-
-        
-        
+        $('.articleSection').empty()
         addedToWatchList[ticker] = {
             Symbol: ticker,
             Name: stock,
             Currency: currency,
-            Open : todaysOpen,
+            Open: todaysOpen,
             Close: todaysClose,
             Changes: todaysChanges,
             ChangePercentage: todaysChangesPercent,
             Volume: todaysVolume
         }
-
         $('.watchList').empty();
-        
         for (let item in addedToWatchList) {
             const currentObject = addedToWatchList[item];
-            for (let key in currentObject){
+            newsApp.init(item);//Gets the news for the watch list items
+            for (let key in currentObject) {
                 if (currentObject[key] != undefined)
-                $('.watchList').append(
-                    `<li>${key}: ${currentObject[key]}</li>`
-                )
+                    $('.watchList').append(
+                        `<li>${key}: ${currentObject[key]}</li>`
+                    )
             }
         }
-
-
     })
 }
-
 //Access Stock News API - 
 newsApp.init = function (query) {
     $.ajax({
@@ -117,42 +95,30 @@ newsApp.init = function (query) {
                 </article>
                 </a>`
             )
-            
         })
     });
 };
-
 // Search Endpoint Suggestions --- CREATE DIV/LI BELOW forEACH result?
 $('.tickerSearchButton').on('click', function () {
     $('.tickerSearchResults').empty();
-    
     const searchItem = $('#searchTickerInput').val();
-
     $('.articleSection').empty();//Empty your articles
-
     stockApp.searchEndpoint(searchItem);
 })
 // So you can submit the answer instead of clicking the submit button
 $('#searchTickerInput').on('keypress', function (e) {
-    if(e.which == 13) {
+    if (e.which == 13) {
         $('.tickerSearchResults').empty();
-        
         const searchItem = $('#searchTickerInput').val();
-        
         $('.articleSection').empty();//Empty your articles
-        
         stockApp.searchEndpoint(searchItem);
     }
-    
 })
-
 // Access information for Ticker
 $('.tickerSearchResults').on('click', '.searchResults', function () {
     const ticker = $(this).attr('id')
     const stock = $(this).children('.resultName').text();
     const currency = $(this).children('.resultCurrency').text();
-
     $(`.tickerSearchResults`).empty()
-
     stockApp.searchStock(ticker, 'daily', stock, currency);
 });
